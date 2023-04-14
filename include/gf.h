e3d06d09072f2cee0bf4641cbadc38a72ecd23ea
stygian-phrygian <stygian-phrygian@users.noreply.github.com>
2023-04-14 02:44:14 -0400

    updated header

--- include/gf.h
+++ include/gf.h
@@ -12,39 +12,24 @@
 namespace gf
 {
 
-
-/*
- * general idea:
- *     an app is one or more screens
- *
- *     a screen is a matrix of cells with a grid overlay
- *
- *     a zone is a segment within that grid that houses some update function
- *
- *     an update function is something that takes user input and mutates the
- *     cells within its zone
- *
- * TODO: how do we deal with multiple zones all rendering concurrently?
- */
-
 using UserInputEvent = int;
-using Cell = char;
-using Screen = std::vector<std::vector<Cell>>; // row by col
 
 // A widget is a user interface element. It knows:
 // - how to update its state from user input events
-// - how to print itself onto a Screen
+// - how to print itself onto an ncurses WINDOW
 struct Widget
 {
     // update state given user input event
     void update(UserInputEvent e);
-    // print view onto screen
+    // print view
     void print(
-            Screen& screen,
-            int top_row,
-            int right_col,
-            int bot_row,
-            int left_col);
+            // the stdscr window
+            WINDOW* window,
+            // absolute position of our view's top left corner on stdscr
+            std::pair<int, int> top_left_corner /*rows cols*/,
+            // our view's dimensions
+            int height,
+            int width);
 };
 
 namespace Grid
@@ -60,15 +45,12 @@ struct Zone
 
 struct Layout
 {
-    Layout(int rows, int cols, Screen& screen) :
+    Layout(int rows, int cols) :
         rows{rows},
-        cols{cols},
-        screen{screen}
+        cols{cols}
     {}
-
     int rows;
     int cols;
-    Screen& screen;
     std::map<Zone, Widget> widgets;
 };
 
@@ -80,10 +62,7 @@ struct App
     App();
     ~App();
     void listen();
-    void focus_layout(Grid::Layout& layout);
-
-    Screen screen;
-    std::set<Grid::Layout> layouts;
+    std::vector<Grid::Layout> layouts;
 };
 
 }
